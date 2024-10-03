256af558e5618fddb93e2cb28e7f78ce362264a1
Ravish Ranjan <122588009+Ravish-Ranjan@users.noreply.github.com>
2024-10-03 15:50:16 +0530

    cleanup

---
+++ sum.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int a,b;
+    printf("Enter the numbers to sum them\n");
+    printf("Number 1 : ");
+    scanf("%d",&a);
+    printf("Number 2 : ");
+    scanf("%d",&b);
+    printf("Sum : %d",a+b);
+    return 0;
+}
